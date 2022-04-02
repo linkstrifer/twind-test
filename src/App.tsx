161f2c2ee5eeb2d7ff95711a3a5178a9cb323aac
Link Strifer <linkstrifer@outlook.com>
2022-04-02 10:36:35 -0500

    fix types errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { tw } from "twind";
+import { tw } from 'twind';
 
 export default function App() {
   return (
     <div
-      className={tw`flex flex(col) justify-center items-center w-screen h-screen bg-blue-400 text-white font-bold`}
+      className={tw`flex flex(col grow) justify-center items-center w-screen h-screen bg-blue-400 text-white font-bold`}
     >
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
