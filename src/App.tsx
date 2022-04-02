f5eb1c928a8e6f17bfd82c6bcbbc9442ab3649a3
Link Strifer <linkstrifer@outlook.com>
2022-04-02 10:22:19 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { tw } from "twind";
+
+export default function App() {
+  return (
+    <div
+      className={tw`flex flex(col) justify-center items-center w-screen h-screen bg-blue-400 text-white font-bold`}
+    >
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
